fdf0b495208f33098d1456a101c314b26a2d2668
Wesley Scholl <clients@konjoweb.com>
2023-01-25 22:40:37 -0500

    Initial Commit

---
+++ Nav/NavApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavApp.swift
+//  Nav
+//
+//  Created by Konjo on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct NavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
